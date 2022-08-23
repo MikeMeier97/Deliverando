7480751d8b9e8aa309f77522712c7db66b6e8f9e
MikeMeier97 <m0.0m58@yahoo.de>
2022-08-23 19:41:05 +0200

    Deliverando

---
+++ js/localStorage.js
@@ -0,0 +1,33 @@
+// Local Storage 
+
+//Speichern Warenkorb
+function saveBasket()  {
+    let addedNamesAsText = JSON.stringify(addedNames);
+    let addedDescriptionsAsText = JSON.stringify(addedDescriptions);
+    let addedPricingsAsText = JSON.stringify(addedPricings);
+    let addedAmountAsText = JSON.stringify(addedAmount);
+
+    localStorage.setItem('addedNames', addedNamesAsText);
+    localStorage.setItem('addedDescriptions', addedDescriptionsAsText);
+    localStorage.setItem('addedPricings', addedPricingsAsText);
+    localStorage.setItem('addedAmount', addedAmountAsText);
+}
+
+// Gespeicherten Warenkorb laden 
+function loadBasket() {
+    let addedNamesAsText = localStorage.getItem('addedNames');
+    if (addedNamesAsText)
+        {addedNames = JSON.parse(addedNamesAsText);}
+
+    let addedDescriptionsAsText = localStorage.getItem('addedDescriptions');
+    if (addedDescriptionsAsText)
+        {addedDescriptions = JSON.parse(addedDescriptionsAsText);}    
+
+    let addedPricingsAsText = localStorage.getItem('addedPricings');
+    if (addedPricingsAsText)
+        {addedPricings = JSON.parse(addedPricingsAsText);}    
+    
+    let addedAmountAsText = localStorage.getItem('addedAmount');
+    if (addedAmountAsText)
+        {addedAmount = JSON.parse(addedAmountAsText)}    
+    }
